80750cc6e995786f91b9399f28f637693f75eda5
Masroor Amir <masrooramir76@gmail.com>
2025-09-05 00:42:17 +0500

    Configured Spring Web Security Credentials - Static Username & Password - In-Memory User Credentials

--- src/main/java/com/baloch/restapi/config/SecurityConfig.java
+++ src/main/java/com/baloch/restapi/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.baloch.restapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,6 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER");
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -22,6 +31,7 @@ public class SecurityConfig {
                  .httpBasic(Customizer.withDefaults());
          return httpSecurity.build();
     }
+
 
 
 }
